f5c9d5a6ff38a2e927fda491a86e660470590b87
Joshua Greenberg <joshua.p.greenberg@gmail.com>
2015-08-10 10:46:55 -0700

    much, much faster

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject clojure-sha-3 "0.1.0"
   :description ""
   :url ""
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [prismatic/hiphip "0.2.0"]]
+  :jvm-opts ^:replace [])
--- src/joshua_g/sha_3.clj
+++ src/joshua_g/sha_3.clj
@@ -1,6 +1,9 @@
 (ns ^{:doc "clojure implementation of SHA-3"
       :author "Joshua Greenberg"}
-  joshua-g.sha-3)
+  joshua-g.sha-3
+  (:require [hiphip.long :as hl])) ; for mutable array sugar
+
+(set! *unchecked-math* true)
 
 (declare keccak-1600)
 (declare round-1600)
@@ -8,14 +11,22 @@
 (declare split-input-into-words)
 (declare word-to-little-endian-bytes)
 
-(def ^:private param-map
-  {:sha-3-512 {:output-size 512
-               :rate 576
-               :domain-suffix 2r10
-               :suffix-len 2}})
+(def ^:private sha-3-base-params
+  {:domain-suffix 2r10, :suffix-len 2})
+
+(def ^:private sha-3-with-size (partial assoc sha-3-base-params :output-size))
+
+(defn sha-3-224 [byte-coll]
+  (keccak-1600 (sha-3-with-size 224) byte-coll))
+
+(defn sha-3-256 [byte-coll]
+  (keccak-1600 (sha-3-with-size 256) byte-coll))
+
+(defn sha-3-384 [byte-coll]
+  (keccak-1600 (sha-3-with-size 384) byte-coll))
 
 (defn sha-3-512 [byte-coll]
-  (keccak-1600 (param-map :sha-3-512) byte-coll))
+  (keccak-1600 (sha-3-with-size 512) byte-coll))
 
 (def ^:private round-constants
   [0x0000000000000001 0x0000000000008082 -0x7FFFFFFFFFFF7F76 -0x7FFFFFFF7FFF8000
@@ -26,27 +37,32 @@
    -0x7FFFFFFF7FFF7F7F -0x7FFFFFFFFFFF7F80 0x0000000080000001 -0x7FFFFFFF7FFF7FF8])
 
 (def ^:private rotation-offsets
-  [0 1 62 28 27 36 44 6 55 20 3 10 43 25 39 41 45 15 21 8 18 2 61 56 14])
+  (long-array
+   [0 1 62 28 27 36 44 6 55 20 3 10 43 25 39 41 45 15 21 8 18 2 61 56 14]))
 
 (def ^:private pi-permutation
-  [0 6 12 18 24 3 9 10 16 22 1 7 13 19 20 4 5 11 17 23 2 8 14 15 21])
+  (long-array
+   [0 6 12 18 24 3 9 10 16 22 1 7 13 19 20 4 5 11 17 23 2 8 14 15 21]))
+
+(defn- calc-bit-rate [params]
+  (- 1600 (* 2 (params :output-size))))
 
 (defn keccak-1600 [params byte-coll]
   "Keccak with b = 1600, i.e. 64-bit words and 24 rounds per block"
   {:pre [(coll? byte-coll)]}
   (let
-      [init-state (vec (repeat 25 0))
-       block-size-in-words  (quot (params :rate) 64)
+      [init-state (repeat 25 0)
+       block-size-in-words  (quot (calc-bit-rate params) 64)
        output-size-in-bytes (quot (params :output-size) 8)
 
        keccak-f
        (fn [state]
-         (reduce round-1600 state round-constants))
+         (reduce round-1600 (long-array state) round-constants))
 
        absorb-block
        (fn [state block]
          (->> (concat block (repeat 0))
-              (mapv bit-xor state)
+              (map bit-xor state)
               keccak-f))
 
        squeeze (partial take block-size-in-words)]
@@ -60,56 +76,67 @@
          (mapcat word-to-little-endian-bytes)
          (take output-size-in-bytes))))
 
-(defn- positive-rotate ^long [^long n ^long x]
-  (bit-or
-   (bit-shift-left x n)
-   (unsigned-bit-shift-right x (- 64 n))))
+(defmacro ^:private positive-rotate [n x]
+  `(let [n# (long ~n)
+         x# (long ~x)]
+     (bit-or
+      (bit-shift-left x# n#)
+      (unsigned-bit-shift-right x# (- 64 n#)))))
 
-(defn- chi-combinator ^long [^long a ^long b ^long c]
-  (bit-xor a (bit-and c (bit-not b))))
+(defmacro ^:private chi-combinator [a b c]
+  `(bit-xor (long ~a) (bit-and (long ~c) (bit-not (long ~b)))))
 
-(defn- round-1600 [state round-constant]
+(defmacro ^:private mod5 [x]
+  `(unchecked-remainder-int (+ (long ~x) 5) 5))
+
+(defn- round-1600 [state-array round-constant]
   (let [theta
-        (fn [state]
-          (let [c (reduce (fn [c i]
-                            (update c (mod i 5) bit-xor (state i)))
-                          [0 0 0 0 0]
-                          (range 25))
-                c-shift (mapv #(positive-rotate 1 %) c)]
-            (map-indexed (fn [i w]
-                           (bit-xor w
-                                    (c (mod (dec i) 5))
-                                    (c-shift (mod (inc i) 5))))
-                         state)))
+        (fn [sa]
+          (let [c (let [c (long-array 5 0)]
+                    (hl/doarr [[i w] sa]
+                              (hl/aset c (mod5 i)
+                                       (bit-xor w (hl/aget c (mod5 i)))))
+                    c)
+                c-rot (hl/amap [x c] (positive-rotate 1 x))]
+            (hl/doarr [[i w] sa]
+                      (hl/aset sa i
+                               (bit-xor w
+                                        (hl/aget c (mod5 (dec i)))
+                                        (hl/aget c-rot (mod5 (inc i))))))
+            sa))
 
         rho
-        (fn [state]
-          (mapv positive-rotate rotation-offsets state))
+        (fn [sa]
+          (hl/doarr [[i w] sa]
+                    (hl/aset sa i
+                             (positive-rotate (hl/aget rotation-offsets i)
+                                              w)))
+          sa)
 
         pi
-        (fn [state]
-          (reduce (fn [s from-index]
-                    (conj s (state from-index)))
-                  []
-                  pi-permutation))
+        (fn [sa]
+          (hl/amake [i 25]
+                    (hl/aget sa (hl/aget pi-permutation i))))
 
         chi
-        (fn [state]
-          (for [y (range 5)
-                x (range 5)]
-            (chi-combinator (state (+ x (* 5 y)))
-                            (state (+ (mod (inc x) 5) (* 5 y)))
-                            (state (+ (mod (+ x 2) 5) (* 5 y))))))
+        (fn [sa]
+          (hl/amake [i 25]
+                    (let [m (long (- i (mod5 i)))]
+                      (chi-combinator
+                       (hl/aget sa i)
+                       (hl/aget sa (+ m (mod5 (+ i 1))))
+                       (hl/aget sa (+ m (mod5 (+ i 2))))))))
 
         iota
-        (fn [state]
-          (update (vec state)
-                  0 bit-xor round-constant))
+        (fn [sa]
+          (hl/aset sa 0
+                   (bit-xor round-constant (hl/aget sa 0)))
+          sa)
         ]
-    (-> state theta rho pi chi iota)))
+    (-> state-array theta rho pi chi iota)))
 
 (defn- pad-input [params input-bytes]
-  (let [block-size-in-bytes (quot (params :rate) 8)
+  (let [block-size-in-bytes (quot (calc-bit-rate params) 8)
         padding-bytes
         (as-> (inc (count input-bytes)) $
               (mod (- $) block-size-in-bytes)
